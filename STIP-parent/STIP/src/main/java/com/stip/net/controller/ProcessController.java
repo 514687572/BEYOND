ca2a7a1a96f9753add93585c27a0fc66a0f29bcc
安爷 <chenjunan@foxmail.com>
2017-07-14 14:07:52 +0800

    控制类采用@RestController注解 以取缔@ResponseBody

--- STIP-parent/STIP/src/main/java/com/stip/net/controller/ProcessController.java
+++ STIP-parent/STIP/src/main/java/com/stip/net/controller/ProcessController.java
@@ -14,14 +14,14 @@ import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Scope("request")
 @RequestMapping("/process")
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = { RuntimeException.class, Exception.class })
--- STIP-parent/STIP/src/main/java/com/stip/net/controller/TestController.java
+++ STIP-parent/STIP/src/main/java/com/stip/net/controller/TestController.java
@@ -1,37 +1,28 @@
 package com.stip.net.controller;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import javax.annotation.Resource;
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.stip.net.entity.MallUser;
 import com.stip.net.service.ITestService;
 import com.stip.net.utils.FileUtils;
 
-@Controller
+@RestController
 @Scope("request")
 @RequestMapping("/test")
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = { RuntimeException.class, Exception.class })
